35daca2a977781ba6ae218e08ecdf8ef1f3c350d
nattanjunior <fnatanieljunior@gmail.com>
2024-10-16 15:37:39 -0300

    notification service implemented in the controller

--- src/controllers/CurrencyLatestController.ts
+++ src/controllers/CurrencyLatestController.ts
@@ -1,5 +1,9 @@
 import { SearchCurrencyLatest } from "../models/SearchCurrencyLatest";
 import { CurrencyLatestService } from "../services/CurrencyLatestService";
+import { NotificationService } from "../services/NotificationService";
+
+
+const notification = new NotificationService();
 
 export class CurrencyLatestController {
   private currencyLatestService: CurrencyLatestService;
@@ -10,10 +14,19 @@ export class CurrencyLatestController {
   }
 
   async handleRequestHistory({ req, res }: any) {
-    const { currency } = req.body;
+    const { currency, userId } = req.body;
     try {
-      const ExchangeRate = await this.currencyLatestService.getExchangeRateLatest(currency)
-      res.json({ ExchangeRate })
+      const ExchangeRate = await this.currencyLatestService.getExchangeRateLatest(currency);
+
+      if (!ExchangeRate) {
+        console.log("rror fetching exchange rate")
+        res.status(404).json({ "message": "Exchange rate not found" })
+      };
+      res.json({ ExchangeRate });
+
+      const message = `The exchange rate for ${currency} has been successfully retrivied.`;
+      notification.sendNotification(userId, message);
+
     } catch (error) {
       console.log(error)
       res.status(500).json({ "message": "Error fetching api" })
