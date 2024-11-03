0ff99749f547f6702fc9fcd840498e90e0d2431c
nattanjunior <fnatanieljunior@gmail.com>
2024-11-03 17:52:34 -0300

    console.log comentado

--- src/models/SearchCurrencyLatest.ts
+++ src/models/SearchCurrencyLatest.ts
@@ -1,18 +1,17 @@
-import type { CurrencyLatestRules } from "../interfaces/CurrencyLatetsRules";
+import type { CurrencyLatestRules } from "../interfaces/CurrencyLatestsRules";
 import { apiKey } from "./SearchCurrencyHistory";
 
 
 export class SearchCurrencyLatest implements CurrencyLatestRules {
   // busca taxas de câmbio recentes
-  async ExchageRateLatest(currency: string): Promise<number> {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${currency}`);
+  async ExchageRateLatest(base_currency: string): Promise<object> {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${base_currency}`);
 
-    if(!response.ok){
+    if (!response.ok) {
       throw new Error(`Error fetching exchange rate: ${response.statusText}`)
     }
-
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     return data.conversion_rates;
   }
 }
