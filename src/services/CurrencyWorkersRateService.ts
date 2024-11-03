8e37c3cfc85fb97137e420be774d85a394ca4e81
nattanjunior <fnatanieljunior@gmail.com>
2024-11-03 17:57:16 -0300

    foi apagado tudo da funcao principal

--- src/services/CurrencyWorkersRateService.ts
+++ src/services/CurrencyWorkersRateService.ts
@@ -1,51 +1,18 @@
 import amqp from "amqplib/callback_api";
-import type { CurrencyLatestRules } from "../interfaces/CurrencyLatetsRules";
+import { CurrencyLatestService } from "../services/CurrencyLatestService";
+import { SearchCurrencyLatest } from "../models/SearchCurrencyLatest";
 
 export class CurrencyWorkersRateService {
 
-  constructor(private CurrencyLatestRules: CurrencyLatestRules) { }
+  private CurrencyLatestService: CurrencyLatestService;
 
-  GetNotificationExchangeRate(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      amqp.connect('amqp://localhost', (error0, connection) => {
-        if (error0) {
-          console.log("Error of conexion of worker:", error0)
-          return;
-        };
-        connection.createChannel((error1, channel) => {
-          if (error1) {
-            console.log("Error to the create channel of worker:", error1);
-            connection.close();
-            return;
-          };
+  constructor() {
+    const SearchExchangeRateLatest = new SearchCurrencyLatest();
+    this.CurrencyLatestService = new CurrencyLatestService(SearchExchangeRateLatest);
+  }
 
-          const exchange = 'exchange_rate_requests';
-          channel.assertExchange(exchange, 'fanout', { durable: true });
-
-          channel.assertQueue('', { exclusive: true }, (error2, q) => {
-            if (error2) {
-              console.log("Error in line:", error2);
-              connection.close();
-              return;
-            };
-
-            channel.bindQueue(q.queue, exchange, '');
-            channel.consume(q.queue, async (message) => {
-              if (message?.content) {
-                const { currency } = JSON.parse(message.content.toString());
-                console.log(`Helper processing exchange rate for: ${currency}`);
-
-
-                const ResponseExchange = await this.CurrencyLatestRules.ExchageRateLatest(currency);
-
-                resolve(ResponseExchange);
-
-              }
-            }, { noAck: true });
-          });
-        });
-      });
-    })
+  async GetNotificationExchangeRate(): Promise<any> {
+   
 
   };
 }
